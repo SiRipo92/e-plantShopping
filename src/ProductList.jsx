bd5f1150a3ca11c3717a28889fa293e41c74bce1
SiRipo92 <ripochesierra@gmail.com>
2024-10-23 11:11:49 -0400

    Fixed add to cart button, now shows shopping cart icon, and in the shopping cart we can see the items (images, price and quantity but still cannot increment, decrement or delete items)

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -228,6 +228,7 @@ function ProductList() {
             ]
         }
     ];
+
     const styleObj={
         backgroundColor: '#4CAF50',
         color: '#fff!important',
@@ -237,12 +238,14 @@ function ProductList() {
         alignItems: 'center',
         fontSize: '20px',
     };
+
     const styleObjUl={
         display: 'flex',
         justifyContent: 'space-between',
         alignItems: 'center',
         width: '1100px',
     };
+
     const styleA={
         color: 'white',
         fontSize: '30px',
@@ -288,20 +291,10 @@ return (
           </div>
         </div>
         <div style={styleObjUl}>
-          <div>
-            <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a>
-          </div>
-          <div>
-            <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
-              <h1 className="cart">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" height="68" width="68">
-                  {/* SVG code */}
-                </svg>
-              </h1>
-            </a>
-          </div>
+            <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
+            <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
         </div>
-      </div>
+    </div>
       {!showCart ? (
         <div className="product-grid">
           {plantsArray.map((category, index) => (
